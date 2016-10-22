64126fa99d25ea1ad5a9549d4066d6103fa9d3fc
WalkerTR <dal.bianco.davide@gmail.com>
2016-10-22 21:25:42 +0200

    Added airlines table

--- visualization/public/diverging_airlines.js
+++
@@ -1,1 +0,0 @@
-var diverging_airlines = [{"name":"Airbus Industrie","nDiverging":1},{"name":"Miramichi Air Service","nDiverging":1},{"name":"Stuttgarter Flugdienst","nDiverging":1},{"name":"Vistajet","nDiverging":3}]
